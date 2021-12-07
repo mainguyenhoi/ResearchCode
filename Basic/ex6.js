bca2e4595196239eb83427a81c6906ef9afe5127
nguyenhoi <hoimainguyen@vietjetair.com>
2021-12-07 20:45:48 +0700

    commit 12/7

---
+++ Basic/ex6.js
@@ -0,0 +1,6 @@
+//Tower of Hanoi
+// /There are three towers.
+// The objective of the game is to move all the disks over to tower #3, 
+//but you can't place a larger disk onto a smaller disk.
+// To play the game or learn more about the Tower of Hanoi, check the Resources tab.
+
